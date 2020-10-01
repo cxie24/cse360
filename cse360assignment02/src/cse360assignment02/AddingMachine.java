f2d2db12effe385c48409943ebe72386b3dc660d
xch97 <xch97@DESKTOP-740F436>
2020-09-30 20:49:45 -0700

    initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+/**  
+* AddingMachine.java - A simple adding machine with no implementation yet
+* @author Chenghao Xie
+* @version 1.0 
+* @see CSE360
+*/ 
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
